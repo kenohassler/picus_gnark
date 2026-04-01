bafd5493b38d5b46fa51a945db7d1a28b0622285
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2026-04-01 16:27:14 +0200

    Revert "update for gnark 0.14"
    This reverts commit 129a791d8f31c6473e3da66ab7b78b76abcc5c93.

--- picus_gnark.go
+++ picus_gnark.go
@@ -74,7 +74,7 @@ func CompilePicus(name string, circuit frontend.Circuit, field *big.Int) {
 		fmt.Fprintf(fInfo, "(extra-constraint %v)\n", x)
 	}
 
-	nR1CS, ok := r1cs.(constraint.R1CS[constraint.U64])
+	nR1CS, ok := r1cs.(constraint.R1CS)
 	if ok {
 		constraints := nR1CS.GetR1Cs()
 		for _, r1c := range constraints {
